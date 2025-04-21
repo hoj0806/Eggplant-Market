f4c16d9493fca432135bd686219da385de452921
홍재훈 <ehswkfn123@naver.com>
2025-04-22 03:04:40 +0900

    feature : 게시물 생성 및 supabase storage에 이미지 저장 기능

--- src/features/product/CreatePostForm.tsx
+++ src/features/product/CreatePostForm.tsx
@@ -4,26 +4,53 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import AddressInput from "./AddressInput";
 import FileInput from "../../ui/FileInput";
+import { useForm } from "react-hook-form";
+import Button from "../../ui/Button";
+import { useCreatePost } from "./useCreatePost";
 
 const CreatePostForm = () => {
+  // 추후에 authenticated 유저만 추가하도록 변경
   const [address, setAddress] = useState("");
 
-  console.log(address);
+  const { register, handleSubmit } = useForm();
+  const { createProductPost } = useCreatePost();
+  function onSubmit(data) {
+    console.log(data);
+    createProductPost({
+      ...data,
+      category: "가전",
+      image: data.image,
+      address,
+    });
+  }
+
   return (
-    <Form>
-      <FormRow label='제목' htmlFor='title'>
-        <Input type='text' id='title' />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label='제목' htmlFor='postTitle'>
+        <Input type='text' id='postTitle' {...register("postTitle")} />
       </FormRow>
+
       <FormRow label='내용' htmlFor='description'>
-        <Input type='text' id='description' />
+        <Input type='text' id='description' {...register("description")} />
       </FormRow>
+
+      <FormRow label='물건이름' htmlFor='productName'>
+        <Input type='text' id='productName' {...register("productName")} />
+      </FormRow>
+
       <FormRow label='가격' htmlFor='price'>
-        <Input type='number' id='price' />
+        <Input type='number' id='price' {...register("price")} />
       </FormRow>
+
       <FormRow label='이미지'>
-        <FileInput id='image' accept='image/*' />
+        <FileInput id='image' accept='image/*' {...register("image")} />
       </FormRow>
+
       <AddressInput setAddress={setAddress} />
+
+      <FormRow>
+        <Button>글 올리기</Button>
+      </FormRow>
     </Form>
   );
 };
---
+++ src/services/apiProducts.ts
@@ -0,0 +1,32 @@
+import supabase, { supabaseUrl } from "../supabase";
+
+export async function createProductPost(newPost) {
+  const images = newPost.image;
+  const imagePaths: string[] = [];
+
+  for (const image of images) {
+    const imageName = `${Date.now()}-${image.name}`.replace(/\//g, "");
+    const { error: uploadError } = await supabase.storage
+      .from("product-image") // 버킷 이름
+      .upload(imageName, image);
+
+    if (uploadError) {
+      throw new Error(`이미지 업로드 실패: ${uploadError.message}`);
+    }
+
+    const publicUrl = `${supabaseUrl}/storage/v1/object/public/product-image/${imageName}`;
+    imagePaths.push(publicUrl);
+  }
+
+  const { data, error } = await supabase
+    .from("products")
+    .insert([{ ...newPost, image: imagePaths }])
+    .select()
+    .maybeSingle(); // 하나만 반환되도록 안전하게 처리
+
+  if (error) {
+    throw new Error("게시물 생성에 실패했습니다!");
+  }
+
+  return data;
+}
