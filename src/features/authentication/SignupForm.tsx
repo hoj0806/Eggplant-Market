e20cebe044c8ae893f3991d0236071b27e07e965
홍재훈 <ehswkfn123@naver.com>
2025-04-22 00:05:15 +0900

    feature : 회원가입 기능 완성

--- src/features/authentication/SignupForm.tsx
+++ src/features/authentication/SignupForm.tsx
@@ -2,29 +2,50 @@ import Input from "../../ui/AuthenticationInput";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
+import { useSignup } from "./useSignup";
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  nickname: string;
+  email: string;
+  password: string;
+};
 
 const SignupForm = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(123);
+  const { signup, isPending } = useSignup();
+
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit = ({ nickname, email, password }) => {
+    signup({
+      nickname,
+      email,
+      password,
+    });
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label='닉네임' htmlFor='nickname'>
-        <Input type='text' id='nickname' />
+        <Input type='text' id='nickname' {...register("nickname")} />
       </FormRow>
       <FormRow label='이메일' htmlFor='email'>
-        <Input type='email' id='email' />
+        <Input type='email' id='email' {...register("email")} />
       </FormRow>
       <FormRow label='비밀번호' htmlFor='password'>
-        <Input type='password' id='password' />
+        <Input type='password' id='password' {...register("password")} />
       </FormRow>
       <FormRow label='비밀번호 확인' htmlFor='passwordConfirm'>
-        <Input type='password' id='passwordConfirm' />
+        <Input
+          type='password'
+          id='passwordConfirm'
+          {...register("passwordConfirm")}
+        />
       </FormRow>
       <FormRow>
-        <Button>회원가입</Button>
+        <Button disabled={isPending}>
+          {isPending ? "회원가입 중..." : "회원가입"}
+        </Button>
       </FormRow>
     </Form>
   );
