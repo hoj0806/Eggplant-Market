abd37f55e9c0ca3c2e6a453100acaa7f8602c4b0
홍재훈 <ehswkfn123@naver.com>
2025-04-21 22:15:09 +0900

    feature : 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/signup", element: <Signup /> },
+]);
 
 function App() {
   const queryClient = new QueryClient({
@@ -14,7 +22,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-      <div>123</div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
