841f54b2cdc62fe34b906d88b1a2a01a8fac34b2
홍재훈 <ehswkfn123@naver.com>
2025-04-26 01:18:36 +0900

    style : header 레이아웃 디자인

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,15 +1,23 @@
+import styled from "styled-components";
 import HeaderProfile from "./HeaderProfile";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 
 const Header = () => {
-  return (
-    <div style={{ display: "flex", gap: "5rem" }}>
-      <Logo />
+  const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+  `;
 
+  return (
+    <StyledHeader>
+      <Logo />
       <Navigation />
       <HeaderProfile />
-    </div>
+    </StyledHeader>
   );
 };
 
