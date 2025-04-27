33c36a4589681011c4538eb2d38433ec600b2624
홍재훈 <ehswkfn123@naver.com>
2025-04-27 20:16:52 +0900

    feature : 특정 유저 프로필로 이동하는 라우터 추가

--- src/features/product/ProductDetail.tsx
+++ src/features/product/ProductDetail.tsx
@@ -4,7 +4,7 @@ import { useDeleteProduct } from "./useDeleteProduct";
 import styled from "styled-components";
 import DetailImageContainer from "../../ui/DetailImageContainer";
 import PostDescription from "./ProductDescription";
-import { useUserProducts } from "./useUserProducts";
+import UserOtherProducts from "../../ui/UserOtherProducts";
 
 const MainContainer = styled.div`
   padding-top: 2rem;
@@ -26,32 +26,36 @@ const DetailLinkBox = styled.div`
 const ProductDetail = () => {
   const { product, isLoading } = useProduct();
   const { deleteProduct, isDeleting } = useDeleteProduct();
-  const { userProducts, isPending: isPending2 } = useUserProducts(
-    product?.sellerId
-  );
+
   const params = useParams();
 
   const handleDeleteProduct = () => {
     deleteProduct(params.productId);
   };
 
-  if (isLoading || isPending2) return <p>상품정보를 불러오고 있습니다...</p>;
+  console.log(product);
+  if (isLoading) return <p>상품정보를 불러오고 있습니다...</p>;
 
-  console.log(userProducts);
   return (
-    <MainContainer>
-      <DetailLinkBox>
-        <Link to='/'>홈</Link>
-        <p>{product.postTitle}</p>
-      </DetailLinkBox>
-      <ProductDetailBox>
-        <DetailImageContainer images={product.image} />
-        <PostDescription />
-      </ProductDetailBox>
-      <button disabled={isDeleting} onClick={handleDeleteProduct}>
-        {isDeleting ? "삭제중입니다..." : "글 삭제하기"}
-      </button>
-    </MainContainer>
+    <>
+      <MainContainer>
+        <DetailLinkBox>
+          <Link to='/'>홈</Link>
+          <p>{product.postTitle}</p>
+        </DetailLinkBox>
+        <ProductDetailBox>
+          <DetailImageContainer images={product.image} />
+          <PostDescription />
+        </ProductDetailBox>
+        <button disabled={isDeleting} onClick={handleDeleteProduct}>
+          {isDeleting ? "삭제중입니다..." : "글 삭제하기"}
+        </button>
+      </MainContainer>
+      <UserOtherProducts
+        nickname={product.sellerNickname}
+        id={product.sellerId}
+      />
+    </>
   );
 };
 
