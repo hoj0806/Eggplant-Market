4e7486bb70615ca7a4f0f7fcb7c8a62224f6c9b6
홍재훈 <ehswkfn123@naver.com>
2025-04-28 00:36:02 +0900

    feature : 내가 보낸 요청 데이터 불러오는 기능

--- src/pages/MyRequests.tsx
+++ src/pages/MyRequests.tsx
@@ -1,4 +1,10 @@
+import { useSentRequests } from "../features/purchaseRequest/useSentRequest";
+
 const MyRequests = () => {
+  const { isLoading, sentRequests } = useSentRequests();
+
+  if (isLoading) return <div>로딩중입니다...</div>;
+  console.log(sentRequests);
   return <div>내 요청 페이지 입니다.</div>;
 };
 
